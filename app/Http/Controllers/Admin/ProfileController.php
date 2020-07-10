9322e927d1f23ef0d6fba18491b19da4e93d9992
mireitanabe <mirei10231023@gmail.com>
2020-07-10 12:08:51 +0000

    kadai17 fix

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -7,11 +7,11 @@ use App\Http\Controllers\Controller;
 
 class ProfileController extends Controller
 {
-    //
     public function add()
     {
       return view('admin.news.create');
     }
+    
     public function create()
     {
       return redirect('admin/profile/create');
@@ -27,13 +27,3 @@ class ProfileController extends Controller
       return redirect('admin/profile/edit');
     }
 }
-
-Route::group(['prefix' => 'admin'], function(){
-  Route::get('news/create',
-  'Admin\NewsController@add');
-  Route::get('news/delete',
-  'Admin\NewsController@delete');
-  Route::get('news/update',
-  'Admin\NewsController@update');
-});
-
